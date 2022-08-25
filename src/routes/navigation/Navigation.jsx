8d016fdfaf2732acb058fe8516b2d4a73dc7f1b9
Allan Fernandes <Fernzallan14@gmail.com>
2022-08-25 18:14:55 +0100

    routing and navigation-bar setup

---
+++ src/routes/navigation/Navigation.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Outlet, Link} from 'react-router-dom'
+import { Fragment } from 'react'
+import {ReactComponent as Logo } from '../../assets/images/crown.svg'
+import '../navigation/navigation.scss'
+
+const Navigation = () => {
+  return (
+    <Fragment>
+        <div className='navigation'>
+          <Link to='/' className='logo-container'>
+            <Logo className='logo'/>
+          </Link>
+          <div className='nav-links-container'>
+            <Link to='/shop' className='nav-link'>
+              Shop
+            </Link>
+          </div>
+        </div>
+        <Outlet />
+    </Fragment>
+  )
+}
+
+export default Navigation
